1a05304ad68316521ad5511b93bbd0044431f77e
bruno ribeiro <brunowagner62@gmail.com>
2018-03-20 01:02:40 -0300

    Implementado os metodos das classe

--- CorridadeCaes/CorridadeCaes.Dominio/Bet.cs
+++ CorridadeCaes/CorridadeCaes.Dominio/Bet.cs
@@ -15,11 +15,28 @@ namespace CorridadeCaes.Dominio
 
         public string PegaDescricao()
         {
-
+            if (apostador != null)
+            {
+                string descricao = apostador.nome + "aposta R$" + montante + " no cachorro numero : " + cachorro;
+                return descricao;
+            }
+            else
+            {
+                string descricao = apostador.nome + " ainda nao apostou";
+                return descricao;
+            }
         }
 
-        public int Pagamento()
+        public int Pagamento(int cachorroVencedor)
         {
+            if (cachorroVencedor == cachorro)
+            {
+                return (montante + montante);
+            }
+            else
+            {
+                return -montante;
+            }
 
         }
     }
